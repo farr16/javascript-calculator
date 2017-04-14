0bacecdb6f6e7f8156961427fe5ef63b73a6e95b
farr16 <msamfarr@outlook.com>
2017-04-14 15:39:12 -0700

    Add handling for decimal points
    Adds code to make sure numbers can only have one decimal point

--- js/calculator.js
+++ js/calculator.js
@@ -9,32 +9,43 @@ window.onload = function() {
 
 var operators = ["+", "-", "*", "/"];
 
+var noDecimalInCurrNumber = true;
+
 function press(key) {
+  var lastChar = val.slice(-1);
+  
   // Handle clear button being pressed
   if(key === 'C') {
+    noDecimalInCurrNumber = true;
     val = '';
   }
   // Handle operator buttons being pressed
   // TODO: Add special exceptions for minus sign turning numbers negative
   //       and for operators replacing other operators
   else if (operators.indexOf(key) > -1) {
+    noDecimalInCurrNumber = true;
     val += key;
   }
   // Handle decimal button being pressed
-  // TODO: Add handling for making sure numbers only have one decimal point
   else if (key === '.') {
-    val += key;
+	if (noDecimalInCurrNumber) {
+      if (operators.indexOf(lastChar) < 0) {
+        noDecimalInCurrNumber = false;
+        val += key;
+	  }
+	}
   }
   // Handle equals button being pressed
   else if (key === '=') {
     val = '' + eval(val);
+	noDecimalInCurrNumber = (val.indexOf('.') < 0);
   }
   // Handle numbers being pressed
   else {
     val += key;
   }
+  
   // TODO: Replace '/' '-' and '*' characters with division sign, minus sign
   //       and multiplication sign respectively for the screen display
-  
   display.value = val;
 }
