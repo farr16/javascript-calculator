2496e0e8790d935345675370f401e62df686e4e6
farr16 <msamfarr@outlook.com>
2017-04-13 21:54:00 -0700

    Add button press test code
    Adds code for testing button on-click methods. Currently code just adds
    the symbol into the screen, except for the clear button.

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,15 @@
 var display;
 
 window.onload = function() {
-	display = document.getElementById('display');
-	display.value = 'Konnichiwa, sekai!';
+  display = document.getElementById('display');
+  display.value = '0';
 };
+
+function press(val) {
+  if(val == 'C') {
+    display.value = '0';
+  }
+  else {
+    display.value += val;
+  }
+}
